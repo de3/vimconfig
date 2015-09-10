6ddd0c474aa8987569d56aa7b2dd4d4fb843ed3a
Edi Firmansyah <naruto_vs_sousuke@yahoo.com>
2015-09-10 16:38:27 +0700

    add instalation script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PWD=${PWD}
+HOME=${HOME}
+VIMRC=.vimrc
+BUNDLES=$PWD"/common_config/bundles.vim"
+COLORS=$PWD"/colors"
+VIMDIR=$HOME"/.vim"
+
+# create folder
+if [ ! -d $VIMDIR"/common_config" ]; then
+  mkdir -p $VIMDIR"/common_config"
+fi
+if [ ! -d $VIMDIR"/colors" ]; then
+  mkdir -p $VIMDIR"/colors"
+fi
+
+# copy config files
+cp $BUNDLES $VIMDIR"/common_config/"
+cp $COLORS"/*" $VIMDIR"/colors/"
